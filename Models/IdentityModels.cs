e1e75c03d946b337351cc4e65a47e7d757186955
BodiaMalg <56348590+BodiaMalg@users.noreply.github.com>
2020-06-22 19:38:54 +0300

    project

---
+++ Models/IdentityModels.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Hotel_Reservation_System.Models
+{
+    /// <summary>
+    /// Tworzeni indetyfikacji uzytkownika
+    /// </summary>
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+
+        public string Names { get; set; }
+        public string Address { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Date { get; set; }
+        
+
+
+       
+
+        public virtual List<Room_Usage> Usages =>
+            new ApplicationDbContext().Room_Usage.ToList().Where(x => x.Guest_Id == Id).ToList();
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        
+
+        public System.Data.Entity.DbSet<Hotel_Reservation_System.Models.Room> Rooms { get; set; }
+
+        public System.Data.Entity.DbSet<Hotel_Reservation_System.Models.Room_Usage> Room_Usage { get; set; }
+    }
+}
